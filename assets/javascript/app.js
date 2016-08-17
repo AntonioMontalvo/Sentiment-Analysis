2d89c9dcc21a1f1369e304392b58be6c917b00fa
andrewnaeve <andrewnaeve@gmail.com>
2016-08-16 19:49:40 -0700

    basic ajax functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,26 @@
-console.log('hello');
+
+
+function articleSearch(term) {
+  var query = term;
+  var queryURLBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+    $.ajax({url: queryURL, method: 'GET'})
+     .done(function (response) {
+       var search = response.docs[0].web_url;
+       extractor(search)
+     })
+ }
+
+
+function extractor(search) {
+
+  var query = search
+  var queryURL = 'http://boilerpipe-web.appspot.com/extract?url=' + query + '&extractor=ArticleExtractor&output=json'
+  $.ajax({url: queryURL, method: 'GET'})
+   .done(function (response) {
+     console.log(response)
+   })
+  }
+
+
+var input = $("#searchbutton").val().trim().split(" ").join("+");
+articleSearch(input)
